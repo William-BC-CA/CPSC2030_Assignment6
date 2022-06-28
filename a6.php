ab888937b4b81b1d80d5e78a605e85a389666ea9
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-28 19:56:09 +0000

    update a6.php

--- a6.php
+++ a6.php
@@ -1,3 +1,14 @@
+<?php 
+  $my_name = "william_chan";
+  $description = "I use computers.";
+  $favourite_animals = ["cheetahs", "fish", "rhinos", "cougars"];
+
+  function the_developer_profile(){
+    echo "Developer Profile" + $my_name;
+    echo "Description: " + $description;
+    echo "His favourite animals are " + $favourite_animals;
+  }
+?>
 <!DOCTYPE html>
 <html style="">
   <head>
