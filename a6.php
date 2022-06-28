d5364fb99935f8393f999695585796bc8db2c77b
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-28 20:06:40 +0000

    php progress

--- a6.php
+++ a6.php
@@ -4,9 +4,23 @@
   $favourite_animals = ["cheetahs", "fish", "rhinos", "cougars"];
 
   function the_developer_profile(){
+    echo "<div>";
     echo "Developer Profile" + $my_name;
     echo "Description: " + $description;
-    echo "His favourite animals are " + $favourite_animals;
+    // echo "<p>His favourite animals are " + $favourite_animals + "</p>";
+    echo "<p>His favourite animals are ";
+    // https://www.w3schools.com/php/php_looping_foreach.asp
+    $counter = 0;
+    foreach ($favourite_animals as $animals){
+      if ($counter == count($favourite_animals)){
+        echo "and " + $animals;
+      }
+      else {
+        echo "$animals, ";
+      }
+      $counter++; // https://www.php.net/manual/en/language.operators.increment.php
+    }
+    echo "</div>";
   }
 ?>
 <!DOCTYPE html>
