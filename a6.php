4ee392a5dd2481ddbe70edc39867b6f6ba5c6a99
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-28 20:57:46 +0000

    fixes

--- a6.php
+++ a6.php
@@ -31,21 +31,24 @@
   function the_color_form(){
     echo "<div class = 'page-color'>";
     echo "<form method = 'POST' action = 'a6.php'>";
-    echo "<p><input type = 'color' name = 'lecolor'>";
-    echo "Which color do you prefer for the text of this page? <input type = 'submit'></input></p>";
+    echo "<input type = 'color' name = 'lecolor'>";
+    echo "Which color do you prefer for the text of this page? <input type = 'submit'></input>";
     echo "</form>";
     echo "</div>";
-    $couleur = $_POST['lecolor'];
+    // $couleur = $_POST['lecolor'];
   }
 
   function get_color(){
     if (isset($_POST['lecolor'])){
-      return $_POST['lecolor'];
+      echo $_POST['lecolor'];
+    }
+    else {
+      return "black";
     }
   }
 ?>
 <!DOCTYPE html>
-<html style="">
+<html style="color: <?php get_color();?>">
   <head>
     <meta charset="utf-8">
     <title>Assigment 6: Hello PHP World!</title>
