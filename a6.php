1a3e8cfb8cab763ce47b3fb7cdb78ec3d7d3756b
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2022-06-28 12:40:32 -0700

    Add files via upload

---
+++ a6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html style="">
+  <head>
+    <meta charset="utf-8">
+    <title>Assigment 6: Hello PHP World!</title>
+
+    <!--<link rel="stylesheet" href="style.css">-->
+    <!--<link rel="stylesheet" href="icomoon/style.css">-->
+
+  </head>
+  <body>
+
+    <header>
+        <h1>Hello PHP World!</h1>
+        <div class="subtitle">
+          An Introduction to Server-Side Scripting.
+        </div>
+
+    </header>
+
+    <main>
+    
+    </main>
+
+  </body>
+</html>
